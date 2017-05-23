7789eecbcf8821e4076e7de6033c8252c9d2c474
Sean Byfield <seanbyfield@Seans-MacBook-Air.local>
2017-05-22 23:58:30 -0400

    master

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <!-- Just this one line. Notice how the HTML file is passed as the only argument for the function -->
-<?php include_once("views/index.handlebars"); ?>
+<?php include_once("./views/index.handlebars"); ?>
